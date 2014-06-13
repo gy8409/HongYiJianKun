6f04be14d7e4ee0560bc7f31454b864b3cd60659
gy <gy8409@gmail.com>
2014-06-13 21:32:21 +0800

    change template from ejs to swig and test if it is worked.

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 var express = require('express');
+var swig = require('swig');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes/routes');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+app.engine('html', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.set('view cache', false);
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -20,10 +23,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'styles')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/styles', express.static(__dirname+'/styles'));
+app.get('/', routes.getIndex);
+app.get('/index', routes.getIndex);
+app.get('/introduction', routes.getIntroduction);
+app.get('/services', routes.getServices);
+app.get('/career', routes.getCareer);
+app.get('/contact', routes.getContact);
+
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,25 @@
+var sidemenu = [
+	    {source: "", innerHTML: "应用开发和维护"},
+	    {source: "", innerHTML: "ERP实施与咨询"},
+	    {source: "", innerHTML: "软件全球化与本地化"},
+	    {source: "", innerHTML: "专业测试与性能"},
+	    {source: "", innerHTML: "前端解决方案"}
+	];
+
 module.exports = {
     getIndex: function(req, res) {
-	res.render('index');
+	res.render('index', {sidebar: sidemenu});
+    },
+    getIntroduction: function(req, res) {
+	res.render('introduction');
+    },
+    getServices: function(req, res) {
+	res.render('services', {siderbar: sidemenu});
+    },
+    getCareer: function(req, res) {
+	res.render('career');
+    },
+    getContact: function(req, res) {
+	res.render('contact');
     }
 }
