88148461aab0e9c2df90a14287e81f6abed11356
gy <gy8409@gmail.com>
2014-07-29 17:08:49 +0800

    set footer at the bottom of pages; fulfill introduction page; add flip highlight on sidebar of introduction page.

--- routes/routes.js
+++ routes/routes.js
@@ -1,22 +1,28 @@
 var utility = require('../public/javascripts/utility');
 
-var sidemenu = [
-	    {source: "", icon:"%", innerHTML: "应用开发和维护"},
-	    {source: "", icon:"!", innerHTML: "ERP实施与咨询"},
-	    {source: "", icon:"\'", innerHTML: "软件全球化与本地化"},
-	    {source: "", icon:"+", innerHTML: "专业测试与性能"},
-	    {source: "", icon:"$", innerHTML: "前端解决方案"}
-	];
+var introduction_sidemenu = [
+    {source: "#summary", innerHTML: "公司简介"},
+    {source: "#team", innerHTML: "团队"},
+    {source: "#milestone", innerHTML: "里程碑"},
+];
+
+var service_sidemenu = [
+    {source: "", icon:"%", innerHTML: "应用开发和维护"},
+    {source: "", icon:"!", innerHTML: "ERP实施与咨询"},
+    {source: "", icon:"\'", innerHTML: "软件全球化与本地化"},
+    {source: "", icon:"+", innerHTML: "专业测试与性能"},
+    {source: "", icon:"$", innerHTML: "前端解决方案"}
+];
 
 module.exports = {
     getIndex: function(req, res) {
-	res.render('index', {sidebar: sidemenu, bodyId: "index"});
+	res.render('index', {sidebar: service_sidemenu, bodyId: "index"});
     },
     getIntroduction: function(req, res) {
-	res.render('introduction', {bodyId: "introduction"});
+	res.render('introduction', {sidebar: introduction_sidemenu, bodyId: "introduction"});
     },
     getServices: function(req, res) {
-	res.render('services', {sidebar: sidemenu, bodyId: "services"});
+	res.render('services', {sidebar: service_sidemenu, bodyId: "services"});
     },
     getCareer: function(req, res) {
 	res.render('career', {bodyId: "career"});
