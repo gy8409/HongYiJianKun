3743046c4825bfd601c630d688909db94394235a
gy <gy8409@gmail.com>
2014-10-17 17:26:13 +0800

    fix for authentication of redis connection.

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var bcrypt = require('bcrypt');
 var redis = require('redis');
 var rtg = require('url').parse(process.env.REDISTOGO_URL);
 var db = redis.createClient(rtg.port, rtg.hostname);
-redis.auth(rtg.auth.split(":")[1]);
+db.auth(rtg.auth.split(":")[1]);
 var router = express.Router();
 
 /* GET users listing. */
